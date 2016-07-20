e9662ea5b09eb84cbb22d4195455a8babe917da8
David Federspiel <david@federnet.com>
2016-07-20 00:23:42 -0400

    Initial training materials around classes.

---
+++ Classes.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Pet p = new Pet();
+            var i = 0;
+            while(i < 5)
+            {
+                p.Speak();
+                i++;
+            }
+            p.Die();
+        }
+    }
+}
---
+++ Classes/Pet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Pet
+    {
+        SpeechSynthesizer synth;
+        static Random rnd = new Random();
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public DateTime Born { get; set; }
+        public string Breed { get; set; }
+
+        string[] sounds = { "One", "Cat", "Works", "Food", "Beep" };
+
+        public Pet()
+        {
+            synth = new SpeechSynthesizer();
+            synth.Speak("I am alive.");
+        }
+        public void Speak()
+        {
+            synth.Speak(sounds[rnd.Next(sounds.Count())]);
+        }
+
+        public void Eat(float amount)
+        {
+            Weight += amount;
+        }
+
+        public void Poop(float amount)
+        {
+            Weight -= amount;
+        }
+
+        public void Die()
+        {
+            synth.Speak("Was this a good life?");
+        }
+    }
+}
